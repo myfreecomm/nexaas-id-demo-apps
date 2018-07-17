2c0e32ee2fb182936c19351fb0a4f7c4b723ae26
Luiz Carlos Buiatte <buiatte@gmail.com>
2018-07-17 16:06:55 -0300

    Add User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_07_17_190459) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "uid"
+    t.string "token"
+    t.string "email"
+    t.string "name"
+    t.string "picture_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
